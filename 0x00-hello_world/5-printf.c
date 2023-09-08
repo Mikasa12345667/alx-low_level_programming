dba6f1a7a907f37ae4b17d6af79f79df337bdbb9
Mikasa12345667 <gmodera6@gmail.com>
2023-09-08 08:21:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-    printf("With proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammer, but the outcome is a pieces of art,\n"
+	return (0);
 }
-
