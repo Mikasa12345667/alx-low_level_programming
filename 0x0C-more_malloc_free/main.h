3b824883e919c793c1caca5cecc4c71f4a86a8e0
Mikasa12345667 <gmodera6@gmail.com>
2023-10-05 07:12:38 -0700

    created main.h file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _print(char *str, int 1);
+void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
