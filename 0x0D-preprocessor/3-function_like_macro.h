2fe41d2477f3cf2944381931ac8335b2887ccbb1
Mikasa12345667 <gmodera6@gmail.com>
2023-10-09 16:52:26 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,8 @@
-#ifndef _3_FUNCTION_LIKE_MARCO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
+/* Define a function-like macro to calculate the absolute value of a number */
 #define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif /* FUNCTION_LIKE_MACRO_H */
+
